Fixed calculation of losses, including issue where disc loss was negative

src/dataset.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import tensorflow.contrib.slim as slim
 
 def get_dataset_iterator(data_dir,
                          batch_size,
@@ -35,19 +36,19 @@ def decode_image(image_buff, height, width, scale_size, data_format):
         data_format (str): The output image data format (NCHW or NHCW)
 
     Return:
-        Tensor[NCHW | NHCW]: A tensor of shaperepresenting the RGB image.
+        Tensor[NCHW | NHCW]: A tensor of shape representing the RGB image.
     """
-    image_arr       = tf.decode_raw(image_buff, out_type=tf.uint8)
-    image_decoded   = tf.reshape(image_arr, tf.stack([height, width, 3], axis=0))
-    image_decoded   = tf.reverse(image_decoded, [-1]) # BGR to RGB
-    image_decoded   = tf.expand_dims(image_decoded, axis=0)
-    image_resized   = tf.image.resize_nearest_neighbor(image_decoded, [scale_size, scale_size])
+    image = tf.decode_raw(image_buff, out_type=tf.uint8)
+    image = tf.reshape(image, tf.stack([height, width, 3], axis=0))
+    image = tf.reverse(image, [-1]) # BGR to RGB
+    image = tf.expand_dims(image, axis=0)
+    image = tf.image.resize_nearest_neighbor(image, [scale_size, scale_size])
     if data_format == 'NCHW':
-        image_resized = tf.transpose(image_resized, [0, 3, 1, 2])
-    image_resized   = tf.squeeze(image_resized)
-    image_converted = tf.to_float(image_resized)
-
-    return image_converted
+        image = tf.transpose(image, [0, 3, 1, 2])
+    image = tf.squeeze(image)
+    image = tf.image.convert_image_dtype(image, tf.int32)
+    image = 2.0 * ((tf.cast(image, tf.float32) / 255.0) - 0.5)
+    return image
 
 def decode_class(label, num_classes):
     return tf.one_hot(label, num_classes, dtype=tf.float32)

src/model/discriminator.py
@@ -16,7 +16,7 @@ class ACGANDiscriminator():
         self.data_format = data_format
         self.image_size  = image_size
 
-    def build_graph(self, output):
+    def build_graph(self, input_tensor):
         """
         Build AC-GAN Discriminator graph.
 
@@ -28,6 +28,8 @@ class ACGANDiscriminator():
             Tensor[None, num_classes]: A tensor representing the classification for the image.
         """
         with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
+            output = tf.reshape(input_tensor, [-1, self.image_size, self.image_size, 3])
+
             num_convolutions = int(math.log(self.image_size, 2) - 1)
 
             with tf.variable_scope('layers'):
@@ -41,7 +43,7 @@ class ACGANDiscriminator():
                 source_output = slim.fully_connected(output, 1)
                 class_output  = slim.fully_connected(output, self.num_classes)
 
-                return source_output, class_output
+                return tf.reshape(source_output, [-1]), class_output
 
     def conv2d(self,
                input_tensor,

src/model/losses.py
@@ -20,16 +20,13 @@ def class_loss(prediction, labels):
 
 def interpolates(real_batch, fake_batch, alpha):
     with tf.variable_scope('interpolates'):
-        real_batch_flat = slim.flatten(real_batch)
-        fake_batch_flat = slim.flatten(fake_batch)
-        differences  = fake_batch_flat - real_batch_flat
-        interpolates = real_batch_flat + (alpha*differences)
-        return tf.reshape(interpolates, tf.shape(real_batch))
+        differences  = fake_batch - real_batch
+        interpolates = real_batch + (alpha*differences)
+        return interpolates
 
-def lambda_gradient_penalty(disc, interpolates, lambda_penalty):
+def lambda_gradient_penalty(interp_disc, interpolates, lambda_penalty):
     with tf.variable_scope('lambda_gradient_penalty'):
-        disc_flat = tf.reshape(disc, [-1])
-        gradients = tf.gradients(disc_flat, [interpolates])
+        gradients = tf.gradients(interp_disc, [interpolates])
 
         gradients = gradients[0]
 
@@ -38,9 +35,9 @@ def lambda_gradient_penalty(disc, interpolates, lambda_penalty):
 
         return lambda_penalty*gradient_penalty
 
-def total_gen_loss(disc_result, class_loss):
+def total_gen_loss(fake_disc, class_loss):
     with tf.variable_scope('total_loss'):
-        gen_cost  = -tf.reduce_mean(disc_result)
+        gen_cost  = -tf.reduce_mean(fake_disc)
         gen_cost += class_loss
         return gen_cost
 

train.py
@@ -3,6 +3,7 @@ from src.model.discriminator import ACGANDiscriminator as Discriminator
 from src.model.generator import ACGANGenerator as Generator
 import src.model.losses as losses
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
 import numpy as np
 from config import config
 
@@ -79,7 +80,13 @@ def main(_):
     gen_summaries  = []
     disc_summaries = []
 
+    disc_summaries.append(tf.summary.image('real', real_batch, max_outputs=10))
+    gen_summaries.append(tf.summary.image('generated', fake_batch, max_outputs=10))
+
     with tf.variable_scope('losses'):
+        real_batch = slim.flatten(real_batch)
+        fake_batch = slim.flatten(fake_batch)
+
         with tf.variable_scope('gen'):
             gen_class_loss, gen_class_accuracy = losses.class_loss(fake_class_disc, fake_labels)
             total_gen_loss = losses.total_gen_loss(fake_disc, gen_class_loss)
@@ -96,7 +103,8 @@ def main(_):
             interpolates = losses.interpolates(real_batch, fake_batch, alpha)
             interp_disc, interp_class_disc = discriminator_instance.build_graph(interpolates)
 
-            lambda_gradient_penalty = losses.lambda_gradient_penalty(interp_disc, interpolates, lambda_penalty=lambda_penalty)
+            lambda_gradient_penalty = losses.lambda_gradient_penalty(interp_disc, interpolates,
+                                                                     lambda_penalty=lambda_penalty)
 
             total_disc_loss = losses.total_disc_loss(real_disc, fake_disc, disc_class_loss, lambda_gradient_penalty)
 
@@ -122,10 +130,6 @@ def main(_):
                                          var_list=disc_vars,
                                          decay_rate=disc_lr_decay_rate,
                                          decay_steps=disc_lr_decay_steps)
-
-
-    disc_summaries.append(tf.summary.image('real', real_batch, max_outputs=10))
-    gen_summaries.append(tf.summary.image('generated', fake_batch, max_outputs=10))
 
     gen_summaries.append(tf.summary.scalar('gen_learning_rate', gen_exp_lr))
     gen_summaries.append(tf.summary.scalar('disc_learning_rate', disc_exp_lr))